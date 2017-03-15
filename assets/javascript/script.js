1aab6fe1b8f2265c76f09fa520d001be7d63d28b
Rutul Patel <rutul-patel@live.com>
2017-03-14 22:43:38 -0700

    Add working js code, w/o firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,25 @@
 $(function() {
+
+    function addEmployeeToHTML(name, destination, time, frequency) {
+        var trow = $("<tr>");
+        var tcolName = $("<td>").text(name);
+        var tcolDest = $("<td>").text(destination);
+        var tcolTime = $("<td>").text(time);
+        var tcolFreq = $("<td>").text(frequency);
+        var tcolMinAway = $("<td>").text(frequency);
+
+        trow.append(tcolName).append(tcolDest).append(tcolTime).append(tcolFreq).append(tcolMinAway);
+        $(".train-data").append(trow);
+    }
+
     $("#submitBtn").on("click", function(event) {
         event.preventDefault();
-        console.log($("#trainName").val());
-        console.log($("#trainDestination").val());
-        console.log($("#trainTime").val());
-        console.log($("#trainFreq").val());
+        var name = $("#trainName").val().trim();
+        var destination = $("#trainDestination").val().trim();
+        var time = $("#trainTime").val().trim();
+        var freq = $("#trainFreq").val();
+        addEmployeeToHTML(name, destination, time, freq);
+        $("[id^=train]").val("");
     });
+
 });
