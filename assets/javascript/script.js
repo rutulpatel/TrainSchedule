3bcd6158f0cd0188be97a776a0d6c4728163ab87
Rutul Patel <rutul-patel@live.com>
2017-03-14 23:03:25 -0700

    Add firebase child_added functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,17 @@
 $(function() {
 
+    //Initialize firebase
+    var config = {
+        apiKey: "AIzaSyAkFnY9KB3kxQDolUKL88SJ0etCPXrbUdg",
+        authDomain: "train-schedule-c4925.firebaseapp.com",
+        databaseURL: "https://train-schedule-c4925.firebaseio.com",
+        storageBucket: "train-schedule-c4925.appspot.com",
+        messagingSenderId: "494060214144"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
     function addEmployeeToHTML(name, destination, time, frequency) {
         var trow = $("<tr>");
         var tcolName = $("<td>").text(name);
@@ -12,13 +24,25 @@ $(function() {
         $(".train-data").append(trow);
     }
 
+    //loads data from database at initial page load and after every change made to the database
+    database.ref().on("child_added", function(cS) { //cS stands for childSnapshot
+        addEmployeeToHTML(cS.val().name, cS.val().destination, cS.val().time, cS.val().frequency);
+    });
+
     $("#submitBtn").on("click", function(event) {
         event.preventDefault();
         var name = $("#trainName").val().trim();
         var destination = $("#trainDestination").val().trim();
         var time = $("#trainTime").val().trim();
         var freq = $("#trainFreq").val();
-        addEmployeeToHTML(name, destination, time, freq);
+        database.ref().push({
+            name: name,
+            destination: destination,
+            time: time,
+            frequency: freq,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        //addEmployeeToHTML(name, destination, time, freq);
         $("[id^=train]").val("");
     });
 
