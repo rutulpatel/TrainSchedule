3cec9c6c0cbc013797243d4d866d7797e6a59e21
Rutul Patel <rutul-patel@live.com>
2017-03-14 21:23:11 -0700

    Add console output command

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,9 @@
 $(function() {
-    $("#submitBtn").on("click", function() {
-
+    $("#submitBtn").on("click", function(event) {
+        event.preventDefault();
+        console.log($("#trainName").val());
+        console.log($("#trainDestination").val());
+        console.log($("#trainTime").val());
+        console.log($("#trainFreq").val());
     });
 });
