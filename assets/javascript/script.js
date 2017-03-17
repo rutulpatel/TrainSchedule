ff31fa92b5875ac1f22e92538579f049e617f93b
Rutul Patel <rutul-patel@live.com>
2017-03-16 23:01:47 -0700

    Add remove button to each row

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,47 +12,63 @@ $(function() {
 
     var database = firebase.database();
 
-    function addEmployeeToHTML(id, name, destination, frequency, startTime) {
-
-        var now = moment();
-        var nextArrival = moment(startTime, "HH:mm").add(parseInt(frequency), 'm').format("HH:mm");
-
-
-        console.log("startTime = " + startTime);
-        while (moment(nextArrival, "HH:mm").isBefore(now)) {
-            nextArrival = moment(nextArrival, "HH:mm").add(parseInt(frequency), 'm').format("HH:mm");
-        }
-
-        console.log(nextArrival);
-
-        var minAway = moment(nextArrival, "HH:mm").subtract(now.format("H"), "H").subtract(now.format("m"), "m").format("m");
-        console.log(minAway);
-
-        var trow = $("<tr>").attr("id", id);
-        var tcolName = $("<td>").text(name);
-        var tcolDest = $("<td>").text(destination);
-        var tcolNextArrivalTime = $("<td>").text(nextArrival);
-        var tcolFreq = $("<td>").text(frequency);
-        var tcolMinAway = $("<td>").text(minAway);
-        trow.append(tcolName).append(tcolDest).append(tcolFreq).append(tcolNextArrivalTime).append(tcolMinAway);
-        $(".train-data").append(trow);
-
+    function removeRow() {
+        console.log(database);
+        database.ref().child($(this).attr("id")).remove();
     }
 
-    //loads data from database at initial page load and after every change made to the database
-    database.ref().on("child_added", function(cS) { //cS stands for childSnapshot
-            addEmployeeToHTML(cS.getKey(), cS.val().name, cS.val().destination, cS.val().frequency, cS.val().time);
-        }),
-        function(errorObj) {
-            console.log("The read failed: " + errorObj.code);
-        };
-    //get called when the data is deleted from the database
-    database.ref().on("child_removed", function(cS) {
-            $("#" + cS.getKey()).remove();
-        }),
-        function(errorObj) {
-            console.log("Error: " + errorObj.code);
-        };
+    function addRow(id, name, destination, frequency, startTime) {
+
+        if (!$("#" + id).length) {
+            var now = moment();
+            var nextArrival = moment(startTime, "HH:mm").add(parseInt(frequency), 'm').format("HH:mm");
+
+
+            console.log("startTime = " + startTime);
+            while (moment(nextArrival, "HH:mm").isBefore(now)) {
+                nextArrival = moment(nextArrival, "HH:mm").add(parseInt(frequency), 'm').format("HH:mm");
+            }
+
+            console.log(nextArrival);
+
+            var minAway = moment(nextArrival, "HH:mm").subtract(now.format("H"), "H").subtract(now.format("m"), "m").format("m");
+            console.log(minAway);
+
+            var trow = $("<tr>");
+            var tcolName = $("<td>").text(name);
+            var tcolDest = $("<td>").text(destination);
+            var tcolNextArrivalTime = $("<td>").text(nextArrival);
+            var tcolFreq = $("<td>").text(frequency);
+            var tcolMinAway = $("<td>").text(minAway);
+            var cancelButton = $("<td>").html($("<i>").attr({ "class": "fa fa-ban", "id": id, "aria-hidden": "true" }));
+            trow.append(cancelButton).append(tcolName).append(tcolDest).append(tcolFreq).append(tcolNextArrivalTime).append(tcolMinAway);
+            $(".train-data").append(trow);
+            $("#" + id).on("click", removeRow);
+        }
+    }
+
+
+    function onChildAdded() {
+        //clear html before loading
+        $(".train-data").html("");
+        //loads data from database at initial page load and after every change made to the database
+        database.ref().on("child_added", function(cS) { //cS stands for childSnapshot
+                addRow(cS.getKey(), cS.val().name, cS.val().destination, cS.val().frequency, cS.val().time);
+            }),
+            function(errorObj) {
+                console.log("The read failed: " + errorObj.code);
+            };
+    }
+
+    function onChildRemoved() {
+        //get called when the data is deleted from the database
+        database.ref().on("child_removed", function(cS) {
+                $("#" + cS.getKey()).parent().parent().remove();
+            }),
+            function(errorObj) {
+                console.log("Error: " + errorObj.code);
+            };
+    }
 
     $("#submitBtn").on("click", function(event) {
         event.preventDefault();
@@ -90,7 +106,9 @@ $(function() {
             });
             $("[id^=train]").val("");
         }
-
-
     });
+
+    onChildAdded();
+    onChildRemoved();
+    setInterval(onChildAdded, 60 * 1000);
 });
